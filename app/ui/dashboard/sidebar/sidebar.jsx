4da53aa7659fca495c5d6f5907eb5e61bb7714d0
quayes007 <abdullah2312091029@diu.edu.bd>
2025-01-12 02:30:48 +0600

    update idebar

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -44,21 +44,21 @@ const menuItems = [
           },
       ],
     },
-    {
-      title: "User",
-      list: [
-        {
-          title: "Settings",
-          path: "/dashboard/settings",
-          icon: <MdOutlineSettings />,
-        },
-        {
-          title: "Help",
-          path: "/dashboard/help",
-          icon: <MdHelpCenter />,
-        },
-      ],
-    },
+    // {
+    //   title: "User",
+    //   list: [
+    //     {
+    //       title: "Settings",
+    //       path: "/dashboard/settings",
+    //       icon: <MdOutlineSettings />,
+    //     },
+    //     {
+    //       title: "Help",
+    //       path: "/dashboard/help",
+    //       icon: <MdHelpCenter />,
+    //     },
+    //   ],
+    // },
   ];
   
 const Sidebar = () => {
